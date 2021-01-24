ae45120f073da197d5e50bc036c1b7fc6a8fa924
Aren <64262547+arencos@users.noreply.github.com>
2021-01-24 22:25:30 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
 @echo off
 echo This will install the dependencies required for PyYtDownload. Press any key to continue...
 pause>nul
-py -m pip install pytube moviepy
+py -m pip install pytube moviepy update-check
 cls
 echo Done. Press any key to exit.
 pause>nul
